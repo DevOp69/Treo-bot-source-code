05e147e4fb22634c4d80ce2107311c02b6d5c085
DevOp69 <108845911+DevOp69@users.noreply.github.com>
2022-11-17 01:14:05 +0530

    Delete db directory

--- db/SuggestDB.js
+++
@@ -1,7 +0,0 @@
-const { model, Schema } = require("mongoose")
-
-module.exports = model("suggestDB", new Schema({
-    Guild: String,
-    MessageID: String,
-    Details: Array
-}))
--- db/Suggestion.js
+++
@@ -1,6 +0,0 @@
-const { model, Schema } = require("mongoose")
-
-module.exports = model("suggestion", new Schema({
-  Guild: String,
-  Channel: String
-}))
